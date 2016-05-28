bc65f1a0681f35925db31979dbfb4cb01e033f4e
eden90267 <eden90267@gmail.com>
2016-05-28 11:31:26 +0800

    add repositoryConfig

---
+++ src/main/java/my/eden/book/config/RepositoryConfig.java
@@ -0,0 +1,17 @@
+package my.eden.book.config;
+
+import my.eden.book.vo.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+/**
+ * Created by eden90267 on 2016/5/28.
+ */
+@Configuration
+public class RepositoryConfig extends RepositoryRestMvcConfiguration {
+    @Override
+    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Book.class);
+    }
+}
