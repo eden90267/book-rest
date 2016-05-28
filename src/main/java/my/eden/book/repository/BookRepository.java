3c7636e86cd9b78b253f300d4f3b220c39216c6a
eden90267 <eden90267@gmail.com>
2016-05-29 01:37:21 +0800

    add findCountByBookName method

--- src/main/java/my/eden/book/repository/BookRepository.java
+++ src/main/java/my/eden/book/repository/BookRepository.java
@@ -2,6 +2,8 @@ package my.eden.book.repository;
 
 import my.eden.book.vo.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
@@ -9,4 +11,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "book", path = "book")
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT COUNT(b) FROM Book b WHERE b.bookName = :bookName")
+    long findCountByBookName(@Param("bookName") String bookName);
+
 }
